928d92f4a87fba88d563da1f783a85ffb14603f8
SunhoKim <92847080+DevsunhoKim@users.noreply.github.com>
2024-02-21 18:40:54 +0900

    김선호(리뷰 작성 수정중)

--- 2nd_Projcet_4Team/src/main/java/com/sist/dao/ReviewDAO.java
+++ 2nd_Projcet_4Team/src/main/java/com/sist/dao/ReviewDAO.java
@@ -1,34 +1,35 @@
 package com.sist.dao;
-import java.util.*;
-import com.sist.mapper.*;
-import com.sist.vo.ReviewVO;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.sist.mapper.ReviewMapper;
+import com.sist.vo.ReviewVO;
 
 @Repository
 public class ReviewDAO {
 	@Autowired
 	private ReviewMapper mapper;
-	
+
 	public List<ReviewVO> reviewListData(int no)
 	{
-		return mapper.reviewListData(no); 
+		return mapper.reviewListData(no);
 	}
-	
+
 	public void reviewInsert(ReviewVO vo)
 	{
 		mapper.reviewInsert(vo);
 	}
-	
+
 	public void reviewUpdate(ReviewVO vo)
 	{
 		mapper.reviewUpdate(vo);
 	}
-	
+
 	public void reviewDelete(int no)
 	{
 		mapper.reviewDelete(no);
 	}
-	
+
 }
