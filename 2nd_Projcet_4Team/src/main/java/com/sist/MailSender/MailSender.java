334da040d26e413cb574680b634b4128dab9ceb5
user <user@DESKTOP-I4DKJ1Q>
2024-02-22 12:50:35 +0900

    김연수(비밀번호찾기 메일 html 변경)

--- 2nd_Projcet_4Team/src/main/java/com/sist/MailSender/MailSender.java
+++ 2nd_Projcet_4Team/src/main/java/com/sist/MailSender/MailSender.java
@@ -31,13 +31,26 @@ public class MailSender {
 			MimeMessage message=new MimeMessage(session);
 			message.setFrom(new InternetAddress(user));
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
-			message.setSubject("[CODEV]임시 비밀번호 입니다."); // 메일 제목
+			message.setSubject("[CODEV]임시 비밀번호 안내 및 계정 안전에 대한 안내"); // 메일 제목
 			String html="<html>"
        		     +"<head>"
        		     +"</head>"
        		     +"<body>"
-       		     +"<h3>임시비밀번호는 <b>"+tempPwd+"</b>입니다.</h3>"
-       		     +"<h3>로그인 후 비밀번호를 변경해주세요.</h3>"
+       		     +"<h1>임시 비밀번호: <b style=\"color:#7918F2;\">"+tempPwd+"</b><br></h1>"
+       		     +"<h3><p>"
+       		     + "안녕하세요,<br>"
+       		     + "<br>"
+       		     + "임시 비밀번호를 안내해드립니다. 아래 임시 비밀번호로 로그인하신 후, 보안 설정에서 새로운 비밀번호를 설정해주시기 바랍니다.<br>"
+       		     + "<br>"
+       		     + "임시 비밀번호: [<b style=\"color:#7918F2;\">"+tempPwd+"</b>]<br>"
+       		     + "<br>"
+       		     + "보안을 위해 이메일을 통한 비밀번호 재설정이 완료된 후에는 반드시 비밀번호를 변경해주시기 바랍니다. 접속 기록을 확인하시고, 본인이 요청하지 않은 경우에는 즉시 저희에게 알려주시기 바랍니다.<br>"
+       		     + "<br>"
+       		     + "더 궁금한 사항이 있으시거나 도움이 필요하시면 언제든지 저희에게 연락해주시기 바랍니다.<br>"
+       		     + "<br>"
+       		     + "감사합니다.<br>"
+       		     + "<br>"
+       		     + "[CODEV] 드림</p></h3>"
        		     +"</body>"
        		     +"</html>";
 			
