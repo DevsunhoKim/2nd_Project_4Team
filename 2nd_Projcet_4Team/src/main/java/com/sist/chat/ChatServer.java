3a7bc1a2c716d912673faf5245c82a472db761cc
red06 <red06@192.168.219.171>
2024-02-24 01:51:35 +0900

    윤새영(채팅)

---
+++ 2nd_Projcet_4Team/src/main/java/com/sist/chat/ChatServer.java
@@ -0,0 +1,49 @@
+package com.sist.chat;
+
+import java.util.*;
+
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+@RequestMapping("/chatting")
+public class ChatServer extends TextWebSocketHandler{
+
+	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		
+		System.out.println("#ChattingHandler, afterConnectionEstablished");
+		sessionList.add(session);
+		
+		System.out.println(session.getPrincipal().getName() + "님이 입장하셨습니다.");
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		System.out.println("#ChattingHandler, handleMessage");
+		System.out.println(session.getId() + ": " + message);
+		
+		for(WebSocketSession s : sessionList) {
+			s.sendMessage(new TextMessage(session.getPrincipal().getName() + ":" + message.getPayload()));
+		}
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		
+		System.out.println("#ChattingHandler, afterConnectionClosed");
+
+		sessionList.remove(session);
+		
+		System.out.println(session.getPrincipal().getName() + "님이 퇴장하셨습니다.");
+	}
+}
--- 2nd_Projcet_4Team/src/main/java/com/sist/web/ChatController.java
+++ 2nd_Projcet_4Team/src/main/java/com/sist/web/ChatController.java
@@ -1,6 +1,7 @@
 package com.sist.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +22,13 @@ public class ChatController {
    public String chat_chat(Model model,HttpSession session)
    {
 	   String nickname=(String)session.getAttribute("nickname");
+	   String userId=(String)session.getAttribute("userId");
+	   //MemberVO user = (MemberVO) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+	   //System.out.println("@ChatController, GET Chat / Username : " + user.getUserName());
 	   System.out.println(nickname);
+	   System.out.println(userId);
 	   model.addAttribute("nickname", nickname);
+	   model.addAttribute("userId", userId);
 	   return "site/chat/chat_test"; // ViewResolver
    }
 }
